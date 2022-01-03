2e2fb3f49cf6f5d6a4f9a0b894bcfc55cb1508b7
kamilo.vasquez <kamilo.vasquez@globant.com>
2022-01-03 16:43:51 -0500

    new utils file

---
+++ src/utils.ts
@@ -0,0 +1,39 @@
+import { writeFile } from 'fs';
+
+export async function saveFile(data: {}[]) {
+  const parsedData = JSON.stringify(data);
+  await writeFile(
+    `./data/${Date.now()}.json`,
+    parsedData,
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
+}
+
+export function cleanDetailsLinks(links: string[], keywords: string[]) {
+  return links
+    .filter(link => keywords.some(keyword => link.includes(keyword)))
+    .filter((link, index, array) => array.indexOf(link) === index);
+}
+
+export function buildCarObj(carData: {
+  brand: string | null;
+  model: string | null;
+  price: string | null;
+  km: string | null;
+  year: string | null;
+  status: string | null;
+  city: string | null;
+  licensePlate: string | null;
+  transmission: string | null;
+  fuel: string | null;
+  cylinderCapacity: string | null;
+  color: string | null;
+  link: string | null;
+  site: string | null;
+}) {
+  return carData;
+}
