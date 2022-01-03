0eaab06e135fa790f40823ef68cfad4d24c317f3
kamilo.vasquez <kamilo.vasquez@globant.com>
2022-01-03 16:43:35 -0500

    updated index

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
 import { chromium } from 'playwright';
 import carroYa from './carro-ya';
+import tuCarro from './tu-carro';
+import { saveFile } from './utils';
 
 async function searchCar() {
+  const startTime = performance.now();
   console.log('Job started');
   const browser = await chromium.launch({
     headless: false,
     timeout: 0,
   });
 
-  console.log('Browser lauched');
-  await carroYa(browser);
+  const scrappedData = [];
 
-  // await browser.close();
-  // console.log('Browser closed');
-  // // await sendEmail(stocks);
-  // console.log('Job finished.');
+  console.log('Browser lauched');
+  scrappedData.push(...(await carroYa(browser)));
+  scrappedData.push(...(await tuCarro(browser)));
+
+  await browser.close();
+  console.log('Browser closed');
+  await saveFile(scrappedData);
+  console.log('Job finished.');
+  const endTime = performance.now();
+  console.log(`Execution time: ${((endTime - startTime) / 1000 / 60).toFixed(2)} minutes`);
 }
 
-searchCar();
-
-// (async () => {
-//   await searchCar();
-//   setInterval(async () => {
-//     await searchCar();
-//   }, 1000 * 60 * 60);
-// })();
+(async () => {
+  await searchCar();
+  // setInterval(async () => {
+  //   await searchCar();
+  // }, 1000 * 60 * 60);
+})();
